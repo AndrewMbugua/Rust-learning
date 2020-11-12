89d6708871c1ad97f79dce31c224999cccce9cff
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-11-12 22:00:37 +0000

    Create Array and vector.rs

---
+++ Array and vector.rs
@@ -0,0 +1,11 @@
+fn main(){
+//array of size 50 elements with 0 as the 1st value
+let x = [0;50];
+print!("{}",x);
+
+
+//simple vectors
+let y = vec!["this","is"];
+print!("{} {}.Length: {}.", y[0],y[1],y.len());
+
+}
