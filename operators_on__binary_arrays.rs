8903af5f122a561c650486e05c213daf7086f011
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-11-14 07:57:18 +0000

    Create operators_on__binary_arrays.rs

---
+++ operators_on__binary_arrays.rs
@@ -0,0 +1,14 @@
+fn main (){
+
+//logical operators on arrays holding binaries
+let mut _a = [true;5];
+let mut _b = [false;5];
+
+//logical and
+print!("{}", _a[2] && _b[2]);
+
+//logical or
+print!("\n{}", _a[2] || _b[2]);
+
+	
+}
