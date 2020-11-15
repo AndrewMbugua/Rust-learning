3a8dd61dce55c18fa416598717f77938d0c8a77b
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-11-15 11:19:54 +0000

    Update hard_coded_low_calculator.rs

--- hard_coded_low_calculator.rs
+++ hard_coded_low_calculator.rs
@@ -6,7 +6,8 @@
      
 fn main() {
 	//hard coded number
-	
+    let mut _num1: i16 = 100;
+    let mut _num2: i16 = 500;
     let number = 1000;
     //binary
     print!("{}",format!("{:b}",number));
@@ -19,6 +20,16 @@ fn main() {
     //octal
     print!("\n{}", format!("{:o}",number));
     //output - 1750
+	
+	//For the 1st portion
+     print!("The binary format is:\n{}",format!("{:b} \n{:b}",_num1,_num2));
+     print!("\nThe sum is:{}",(_num1 + _num2));
+	//output 
+	/*The binary format is:
+         1100100 
+         111110100
+        The sum is:600
+         */
      
  }
  
