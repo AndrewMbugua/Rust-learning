c85b527dbcc3d03b60146e9f5117a362264e7530
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-11-16 22:19:25 +0000

    Update enumerated_example.rs

--- enumerated_example.rs
+++ enumerated_example.rs
@@ -16,5 +16,24 @@ fn main(){
 		Continent::America => print!("Am"),
 		Continent::Oceania => print!("O"),
 	}
+	//2nd example within one program
+	enum Result {
+	Success(f64),
+	Failure(u16,char),
+	Uncertainty,
+}
+// let outcome = Result::Success(23.67);
+let outcome = Result::Failure(1200,'X');
+
+match outcome {
+	Result::Success(value) => 
+	print!("Result:{}",value),
+	Result::Failure(error_code,module) =>
+	print!("Error n.{} in module{}",
+	error_code,module),
 	
+	Result::Uncertainty => {},
+	
+}
+//Result Error n.1200 in moduleX
 }
