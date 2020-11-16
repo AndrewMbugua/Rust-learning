3d1bef0f3f2bba0025a5fe1ba25bc7c6f0c3e68a
Andrew Mbugua <51198360+AndrewMbugua@users.noreply.github.com>
2020-11-16 19:40:24 +0000

    Create enumerated_example.rs

---
+++ enumerated_example.rs
@@ -0,0 +1,20 @@
+fn main(){
+	
+	enum Continent {
+		Europe,
+		Asia,
+		Africa,
+		America,
+		Oceania,
+	}
+	let contin = Continent::Asia;
+	
+	match contin {
+		Continent::Europe => print!("E"), 
+		Continent::Asia => print!("As"),
+		Continent::Africa => print!("Af"),
+		Continent::America => print!("Am"),
+		Continent::Oceania => print!("O"),
+	}
+	
+}
