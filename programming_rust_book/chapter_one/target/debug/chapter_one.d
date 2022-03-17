e5e0140b80a5a3d93f4ffa9ef0dadc3c3d3e9434
Andrew <andrewmbugua388@gmail.com>
2022-03-17 14:26:05 +0300

    push

--- programming_rust_book/chapter_one/target/debug/chapter_one.d
+++ programming_rust_book/chapter_one/target/debug/chapter_one.d
@@ -1,1 +1,1 @@
-/home/andrew/Documents/Rust-learning/beginning_rust/chapter_one/target/debug/chapter_one: /home/andrew/Documents/Rust-learning/beginning_rust/chapter_one/src/main.rs
+/home/andrew/Documents/Rust-learning/programming_rust_book/chapter_one/target/debug/chapter_one: /home/andrew/Documents/Rust-learning/programming_rust_book/chapter_one/src/main.rs
