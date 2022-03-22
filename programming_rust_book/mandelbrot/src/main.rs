364d2b53a2e8e65ca6712a2bff8025a19a4d6eae
AndrewMbugua <andrewmbugua388@gmail.com>
2022-03-22 19:27:30 +0000

    Scripted auto-commit on change (2022-03-22 19:27:30) by gitwatch.sh

--- programming_rust_book/mandelbrot/src/main.rs
+++ programming_rust_book/mandelbrot/src/main.rs
@@ -1,7 +1,12 @@
-fn square_loop(mut x: f64){
-   loop {
-       x = x * x;
-   }
+extern crate num;
+use num::Complex;
+
+#[allow(dead_code)]
+fn complex_square_add_loop(c: Complex<f64>){
+    let mut z = Complex {re: 0.0, im: 0.0};
+    loop {
+        z = z * z + c;
+    }
 }
 
 fn main() {
