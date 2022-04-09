dd168c130dec417bddd363ab917279b512a0ac91
AndrewMbugua <andrewmbugua388@gmail.com>
2022-04-09 13:04:01 +0300

    Scripted auto-commit on change (2022-04-09 13:04:01) by gitwatch.sh

---
+++ programming_rust_book/practise/target/debug/deps/practise-a563a4489c932958.d
@@ -0,0 +1,5 @@
+/home/andrew/Documents/Rust-learning/programming_rust_book/practise/target/debug/deps/practise-a563a4489c932958: src/main.rs
+
+/home/andrew/Documents/Rust-learning/programming_rust_book/practise/target/debug/deps/practise-a563a4489c932958.d: src/main.rs
+
+src/main.rs:
---
+++ programming_rust_book/practise/target/debug/practise.d
@@ -0,0 +1,1 @@
+/home/andrew/Documents/Rust-learning/programming_rust_book/practise/target/debug/practise: /home/andrew/Documents/Rust-learning/programming_rust_book/practise/src/main.rs
