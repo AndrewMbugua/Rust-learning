5dfab2a1e3a8314df9d1f9bb5cf4220035cc533a
Andrew <andrewmbugua388@gmail.com>
2022-04-09 17:58:28 +0300

    push

--- rust_by_example/rbe/src/main.rs
+++ rust_by_example/rbe/src/main.rs
@@ -1,6 +1,16 @@
+fn reverse(pair: (i32, bool)) -> (bool,i32){
+
+
+let (integer,boolean) = pair;
+
+(boolean,integer)
+}
+
+
 fn main(){
 
+let air = (1,true);
 
-
+println!("{:?}",reverse(air));
 
 }
