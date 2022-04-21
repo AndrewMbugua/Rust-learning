91c772e274c1484ed1c53509ae5d4ceab4d48bac
Andrew <andrewmbugua388@gmail.com>
2022-04-21 18:29:10 +0300

    push

--- programming_rust_book/practise/src/main.rs
+++ programming_rust_book/practise/src/main.rs
@@ -1,9 +1,9 @@
-fn split_at(&self, mid: usize) -> (&str, &str);
 
 fn main(){
 
-
-
+let mut chaos = [3,4,5,5,6];
+println!("{:?}", chaos.sort());
 
 }
+
 
---
+++ programming_rust_book/practise/target/debug/deps/practise-4b2d83fca8f1ef19.d
@@ -0,0 +1,5 @@
+/home/andrew/Documents/Rust-learning/programming_rust_book/practise/target/debug/deps/practise-4b2d83fca8f1ef19.rmeta: src/main.rs
+
+/home/andrew/Documents/Rust-learning/programming_rust_book/practise/target/debug/deps/practise-4b2d83fca8f1ef19.d: src/main.rs
+
+src/main.rs:
