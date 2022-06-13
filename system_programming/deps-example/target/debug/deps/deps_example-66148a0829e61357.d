ff54f3f162424f9d01d109b642284858c637173e
AndrewMbugua <andrewmbugua388@gmail.com>
2022-06-13 20:20:08 +0300

    Scripted auto-commit on change (2022-06-13 20:20:08) by gitwatch.sh

---
+++ system_programming/deps-example/target/debug/deps/deps_example-66148a0829e61357.d
@@ -0,0 +1,5 @@
+/home/andrew/Documents/Rust-learning/system_programming/deps-example/target/debug/deps/deps_example-66148a0829e61357.rmeta: src/main.rs
+
+/home/andrew/Documents/Rust-learning/system_programming/deps-example/target/debug/deps/deps_example-66148a0829e61357.d: src/main.rs
+
+src/main.rs:
---
+++ system_programming/deps-example/target/debug/deps/num_integer-58d9715a81c629b4.d
@@ -0,0 +1,7 @@
+/home/andrew/Documents/Rust-learning/system_programming/deps-example/target/debug/deps/num_integer-58d9715a81c629b4.rmeta: /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/lib.rs /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/roots.rs /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/average.rs
+
+/home/andrew/Documents/Rust-learning/system_programming/deps-example/target/debug/deps/num_integer-58d9715a81c629b4.d: /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/lib.rs /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/roots.rs /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/average.rs
+
+/home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/lib.rs:
+/home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/roots.rs:
+/home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/num-integer-0.1.45/src/average.rs:
