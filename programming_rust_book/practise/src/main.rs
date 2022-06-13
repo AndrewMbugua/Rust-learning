9bcf8a0bf8a66f1f8b639f2e7451365140f893db
Andrew <andrewmbugua388@gmail.com>
2022-06-13 19:24:14 +0300

    push

--- programming_rust_book/practise/src/main.rs
+++ programming_rust_book/practise/src/main.rs
@@ -1,9 +1,18 @@
+struct Person{
+    name: String,
+    birth: i32
+}
+
+
 fn main(){
 
+let mut compos = Vec::new();
+compos.push(Person { name: "Andrew".to_string(), birth: 2000 });
 
-format!("{}","Hedsds");
+for compos_ptr in &compos{
+
+println!("{},{}",compos_ptr.name, compos_ptr.birth);
 
 
-
-
+}
 }
