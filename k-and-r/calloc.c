afc55aa513150b72911c341eec711523d1f9c4ac
AndrewMbugua <andrewmbugua388@gmail.com>
2022-09-28 10:53:43 +0300

    add

---
+++ k-and-r/calloc.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+typedef struct User{
+    int sal;
+    int id;
+}user;
+
+int main(){
+
+user *puser = NULL;
+int count = 10;
+
+puser = calloc(10, sizeof(user));
+
+
+}
