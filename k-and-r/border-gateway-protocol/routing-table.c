2b5798df3df8ce5e85df9f4607e120fe7d6988be
AndrewMbugua <andrewmbugua388@gmail.com>
2022-10-13 13:28:26 +0300

    New changes

--- k-and-r/border-gateway-protocol/routing-table.c
+++ k-and-r/border-gateway-protocol/routing-table.c
@@ -5,25 +5,29 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+#include<asm-generic/types.h> //to use with __u32
 
 typedef struct routing_table
 {
 
 struct routing_table *source_addr; //parent
-__uint8_t hop_count;
+__u8 hop_count;
 struct routing_table *interface;
+__u8 mask;
 
 
 }routing_table;
 
-routing_table *create_table_entry(int source_addr, __uint8_t hop_count, int interface)
+routing_table *create_table_entry(__u32 source_addr, __u8 hop_count, __u8 interface, __u8 mask)
 {
-    routing_table *table_entry = malloc(sizeof(routing_table));
+    routing_table *table_entry = malloc(sizeof(routing_table)); //check for initialization error here
+    
     if(table_entry != NULL)
     {
         table_entry -> hop_count = 0;
         table_entry -> interface = NULL;
         table_entry -> source_addr;
+        table_entry -> mask;
     }
     return table_entry;
 }
@@ -36,7 +40,7 @@ void print_tree(routing_table *root)
 printf("|Hop count| \n %d\n",root -> hop_count);
 
 printf("\n|Interface|\n");
-print_tree(root -> interface);
+print_tree(root -> interface); //recursion
 
 printf("\n|Source address|\n");
 print_tree(root -> source_addr);
@@ -56,7 +60,7 @@ printf("IP address:%d\n",inet_addr("127.0.0.1") );
 
 
 //node 1 - first table entry
-routing_table *entry1 = create_table_entry(10,0,200);
+routing_table *entry1 = create_table_entry(10,0,200,10);
 
 //parent node
 entry1 -> interface = NULL; //NULL for now
